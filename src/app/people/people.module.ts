d6c3bc7ee215026f70e76da1404af0ab0effb7b7
Marit Kristine Moe <marit.moe2@ntebb.no>
2024-03-13 08:46:48 +0100

    Initial project.

---
+++ src/app/people/people.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PeopleListComponent } from './people-list/people-list.component';
+import { PersonItemComponent } from './person-item/person-item.component';
+
+
+
+@NgModule({
+  declarations: [
+    PeopleListComponent,
+    PersonItemComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PeopleModule { }
